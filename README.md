66329994e540baf65dadf5cab74900a8b731eaf5
GurneeshBudhiraja <gurneeshbudhiraja@gmail.com>
2023-12-09 02:06:08 -0500

    readme_updated

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Tic-Tac-Toe
+
+# Tic-tac-toe
+
+A simple Tic-tac-toe game with a responsive design.
+
+
+
+
+
+## Features
+
+- Responsive Design
+- Intuitive User Interface
+- Code Modularity
+- Glassmorphism Design
+
+
+
+
+## Screenshots
+![App Screenshot](https://github.com/GurneeshBudhiraja/Tic-Tac-Toe/assets/140137709/2efbacf4-757f-4656-860c-2a0e128fad0d)
+
+![App Screenshot](https://github.com/GurneeshBudhiraja/Tic-Tac-Toe/assets/140137709/6f41b6ef-ed45-47f7-802c-bac2344d26de)
+
+## Credits
+
+The image used in this project is sourced from https://www.freepik.com/vectors
--- script.js
+++ script.js
@@ -1,82 +1,82 @@
-const MAX_MOVES = 9; //checking the draw
+  const MAX_MOVES = 9; //checking the draw
 
-let para = document.querySelector('.result');
-let turnX = false;
-let reset = document.querySelector('.reset-button');
-let count = 0;
-const buttons = document.querySelectorAll('.box');
-const winningPattern = [
-    [0, 1, 2],
-    [0, 3, 6],
-    [0, 4, 8],
-    [1, 4, 7],
-    [2, 5, 8],
-    [2, 4, 6],
-    [3, 4, 5],
-    [6, 7, 8],
-];
+  let para = document.querySelector('.result');
+  let turnX = false;
+  let reset = document.querySelector('.reset-button');
+  let count = 0;
+  const buttons = document.querySelectorAll('.box');
+  const winningPattern = [
+      [0, 1, 2],
+      [0, 3, 6],
+      [0, 4, 8],
+      [1, 4, 7],
+      [2, 5, 8],
+      [2, 4, 6],
+      [3, 4, 5],
+      [6, 7, 8],
+  ];
 
 
-buttons.forEach((button) => {
-  button.addEventListener('click',()=> {
-    if (!turnX) {
-      button.innerText = 'O';
-      turnX= true;      
-    } else {
-      button.innerText = 'X';
-      turnX= false;
-    }
-    count++;
-    let isWinner = checkWinner();
-    if (isWinner) {
-      count = 0;
-      printWinner(isWinner);
-    }
-    if (count===MAX_MOVES && (!isWinner)) {
-      count = 0;
-      drawMessage();
-    }
-    button.disabled = true;
+  buttons.forEach((button) => {
+    button.addEventListener('click',()=> {
+      if (!turnX) {
+        button.innerText = 'O';
+        turnX= true;      
+      } else {
+        button.innerText = 'X';
+        turnX= false;
+      }
+      count++;
+      let isWinner = checkWinner();
+      if (isWinner) {
+        count = 0;
+        printWinner(isWinner);
+      }
+      if (count===MAX_MOVES && (!isWinner)) {
+        count = 0;
+        drawMessage();
+      }
+      button.disabled = true;
+    });
   });
-});
 
-const checkWinner = ()=> {
-  for (let pattern of winningPattern) {
-    let val1 = buttons[pattern[0]].innerText;
-    let val2 = buttons[pattern[1]].innerText;
-    let val3 = buttons[pattern[2]].innerText;
-    if (val1 !== '' && val2 !== '' && val3 !== '') {
-      if (val1===val2 && val2 === val3){
-        return val1;
+  const checkWinner = ()=> {
+    for (let pattern of winningPattern) {
+      let val1 = buttons[pattern[0]].innerText;
+      let val2 = buttons[pattern[1]].innerText;
+      let val3 = buttons[pattern[2]].innerText;
+      if (val1 !== '' && val2 !== '' && val3 !== '') {
+        if (val1===val2 && val2 === val3){
+          return val1;
+        }
       }
     }
+  };
+
+  const drawMessage = () => {
+    para.classList.remove('reset');
+    para.innerText = 'Its A Draw';
+    
   }
-};
 
-const drawMessage = () => {
-  para.classList.remove('reset');
-  para.innerText = 'Its A Draw';
-  
-}
+  const printWinner = (isWinner) => {
+    para.classList.remove('reset');
+    reset.classList.remove('reset');
+    para.innerText = `The Winner is ${isWinner}`;
+    disableButtons();
+  }
+  const disableButtons = () => {
+    buttons.forEach((button) => {
+      button.disabled= true;
+    });
+  };
 
-const printWinner = (isWinner) => {
-  para.classList.remove('reset');
-  reset.classList.remove('reset');
-  para.innerText = `The Winner is ${isWinner}`;
-  disableButtons();
-}
-const disableButtons = () => {
-  buttons.forEach((button) => {
-    button.disabled= true;
+  reset.addEventListener('click',() => {
+    buttons.forEach(button => {
+      button.disabled=false;
+      button.innerText='';
+    });
+    count = 0;
+    para.classList.add('reset');
+    turnX = false; 
   });
-};
-
-reset.addEventListener('click',() => {
-  buttons.forEach(button => {
-    button.disabled=false;
-    button.innerText='';
-  });
-  count = 0;
-  para.classList.add('reset');
-  turnX = false; 
-});
