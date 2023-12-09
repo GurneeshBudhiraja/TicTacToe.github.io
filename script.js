506e464927287240afd3c28eda4a645c6c22df5a
GurneeshBudhiraja <gurneeshbudhiraja@gmail.com>
2023-12-09 00:15:16 -0500

    tictactoePC

--- script.js
+++ script.js
@@ -1,79 +1,83 @@
-let buttons = document.querySelectorAll('.boxes');
-let resetButton = document.querySelector('.reset');
-const para = document.querySelector('p');
+const MAX_MOVES = 9; //checking the draw
+
+let para = document.querySelector('.result');
+let turnX = false;
+let reset = document.querySelector('.reset-button');
 let count = 0;
-let cross = false; //checking the turn
+const buttons = document.querySelectorAll('.box');
 const winningPattern = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-]; //array for checking the winner
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+];
 
+para.classList.add('reset');
 
-buttons.forEach(button => {
-  button.addEventListener('click', ()=>{
-    count++;
-    if (!cross) {
-      console.log(button);
+buttons.forEach((button) => {
+  button.addEventListener('click',()=> {
+    if (!turnX) {
       button.innerText = 'O';
-      cross=true;
+      turnX= true;      
     } else {
-      console.log(button);
       button.innerText = 'X';
-      cross= false;
+      turnX= false;
+    }
+    count++;
+    let isWinner = checkWinner();
+    if (isWinner) {
+      count = 0;
+      printWinner(isWinner);
+    }
+    if (count===MAX_MOVES && (!isWinner)) {
+      count = 0;
+      drawMessage();
     }
     button.disabled = true;
-    let gameWinner = checkGameWinner();
-    if (gameWinner) {
-      para.classList.replace('reset-para','show-para');
-      para.innerText = `The Winner is ${gameWinner}`;
-      resetButton.classList.replace('reset','reset-button');
-      disableButtons();
-    } 
   });
 });
 
-
-// disabling all the buttons
-
-const disableButtons = () => {
-buttons.forEach(button => {
-  button.disabled=true;
-});
-};
-
-const checkGameWinner = () => {
+const checkWinner = ()=> {
   for (let pattern of winningPattern) {
     let val1 = buttons[pattern[0]].innerText;
     let val2 = buttons[pattern[1]].innerText;
     let val3 = buttons[pattern[2]].innerText;
-    if (val1!=='' && val2!=='' && val3!=='' && count!==9) {
-      if (val1===val2 && val1===val3) {
+    if (val1 !== '' && val2 !== '' && val3 !== '') {
+      if (val1===val2 && val2 === val3){
         return val1;
       }
-    } else if (count===9) {
-      drawMessage();
     }
   }
 };
 
-resetButton.addEventListener('click', ()=> {
-  buttons.forEach(button => {
-    button.innerText = '';
-    button.disabled= false;
-  });
-  resetButton.classList.replace('reset-button','reset');
-  para.classList.replace('show-para','reset-para');
-});
-
 const drawMessage = () => {
-  para.classList.replace('reset-para','show-para');
-  para.innerText = 'The Match Has Been Drawn!';
-  resetButton.classList.replace('reset','reset-button');
+  para.classList.remove('reset');
+  para.innerText = 'Draw';
+  
+}
+
+const printWinner = (isWinner) => {
+  para.classList.remove('reset');
+  reset.classList.remove('reset');
+  para.innerText = `The Winner is ${isWinner}`;
   disableButtons();
+}
+const disableButtons = () => {
+  buttons.forEach((button) => {
+    button.disabled= true;
+  });
 };
+
+reset.addEventListener('click',() => {
+  buttons.forEach(button => {
+    button.disabled=false;
+    button.innerText='';
+  });
+  count = 0;
+  para.classList.add('reset');
+  turnX = false; 
+});
